f716f657b91f6317866906756947eec79b3ee278
ucabaur <ambroisel@theodo.co.uk>
2023-12-22 17:08:22 +0100

    add dashboard overview

---
+++ frontend/src/app/auth/signIn/page.tsx
@@ -0,0 +1,59 @@
+import { Logo } from "@/components/Logo";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: "Authentication form to energy simulator",
+};
+
+export default function AuthenticationPage() {
+  return (
+    <>
+      <div className="container relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div
+            className="absolute inset-0"
+            style={{
+              backgroundImage:
+                "linear-gradient(to bottom, rgba(19, 19, 17, 0.8),rgba(252, 252, 253, 0)), url(/energy-simulator-cover.webp)",
+              backgroundRepeat: "no-repeat",
+              backgroundAttachment: "fixed",
+              backgroundPosition: "center",
+            }}
+          />
+          <Logo />
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Sign in to your account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email below to sign in to your account
+              </p>
+            </div>
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                href="/terms-and-conditions"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms and conditions
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="/privacy-policy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/app/dashboard/overview/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Energy Sim | Overview",
+  description: "Overview to energy simulator",
+};
+
+export default function OverviewPage() {
+  return <><p>Hello</p></>;
+}
